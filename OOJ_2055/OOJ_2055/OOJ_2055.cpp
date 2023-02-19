5fa1f8286dbdb4a13dd3c7e2dcaf20d81eb338ec
PAMCH <pamic7124@naver.com>
2023-02-19 10:55:41 +0900

    Solved

---
+++ OOJ_2055/OOJ_2055/OOJ_2055.cpp
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARINIGS
+#include <iostream>
+
+using namespace std;
+
+int a, b;
+
+void input() {
+    cin >> a >> b;
+}
+
+void printNum(int w, int  num) {
+    cout.width(w);
+    cout << num << endl;
+}
+
+void printBar() {
+    cout << "------" << "\n";
+}
+
+void output() {
+    printNum(6, a);
+    printNum(6, b);
+    printBar();
+    printNum(6, a*(b%10));
+    printNum(5, a * ((b / 10) % 10));
+    printNum(4, a * (b / 100));
+    printBar();
+    printNum(6, a * b);
+}
+
+int main()
+{
+    input();
+    output();
+
+    return 0;
+}
