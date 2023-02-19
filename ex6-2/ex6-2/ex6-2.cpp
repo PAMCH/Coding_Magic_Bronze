9e76a61c2bdb6b2a96668dea7b3e24da228095f7
PAMCH <pamic7124@naver.com>
2023-02-19 19:20:55 +0900

    Solved

---
+++ ex6-2/ex6-2/ex6-2.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int a[11];
+
+	for (int i = 1; i <= 10; i++)
+		cin >> a[i];
+
+	sort(a + 1, a + 11, greater<>());
+
+	for (int i = 1; i <= 10; i++)
+		cout << a[i] << " ";
+	cout << "\n";
+
+	return 0;
+}
