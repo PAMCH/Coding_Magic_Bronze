40a7c287dd6d12f1e3a8760c7f67c860300db9d7
PAMCH <pamic7124@naver.com>
2023-02-19 19:18:41 +0900

    Solved

---
+++ ex6-1/ex6-1/ex6-1.cpp
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+bool cmp(const int& x, const int& y) {
+	return x > y;
+}
+
+int main()
+{
+	int a[11];
+
+	for (int i = 1; i <= 10; i++)
+		cin >> a[i];
+
+	sort(a + 1, a + 11, cmp);
+
+	for (int i = 1; i <= 10; i++)
+		cout << a[i] << " ";
+	cout << "\n";
+
+	return 0;
+}
