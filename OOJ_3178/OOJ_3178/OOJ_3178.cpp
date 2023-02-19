620c293d9ad1dfe3cd89aee52c5cf273456aa3e1
PAMCH <pamic7124@naver.com>
2023-02-19 18:32:43 +0900

    Solved

---
+++ OOJ_3178/OOJ_3178/OOJ_3178.cpp
@@ -0,0 +1,47 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+#define N 500
+
+using namespace std;
+
+char a[N + 2];
+
+void input() {
+	cin >> a + 1;
+}
+
+bool isOk(const char* str) {
+	int open = 0, close = 0;
+
+	int n = strlen(str + 1);
+	for (int i = 1; i <= n; i++) {
+		if (str[i] == '(') {
+			open++;
+		}
+		else {
+			close++;
+		}
+
+		if (open < close)
+			return false;
+	}
+
+	return open == close;
+}
+
+int main()
+{
+	for (int i = 1; i <= 2; i++) {
+		input();
+		if (isOk(a)) {
+			cout << "YES" << endl;
+		}
+		else {
+			cout << "NO" << "\n";
+		}
+	}
+
+
+	return 0;
+}
