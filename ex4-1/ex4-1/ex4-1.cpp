a232f636901f3e0ca6df400901fa2acdfac49aa6
PAMCH <pamic7124@naver.com>
2023-02-19 18:26:10 +0900

    Solved

---
+++ ex4-1/ex4-1/ex4-1.cpp
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x1, y1, x2, y2, x, y;
+
+	cin >> x1 >> y1 >> x2 >> y2;
+	cin >> x >> y;
+
+	bool b1 = x1 <= x && x <= x2;
+	bool b2 = y1 <= y && y <= y2;
+
+	cout << boolalpha << (b1 && b2) << endl;
+
+
+	return 0;
+}
