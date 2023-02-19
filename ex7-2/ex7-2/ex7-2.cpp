84a321f49122d9b6bea5665d8a9fd1eb56c45fa2
PAMCH <pamic7124@naver.com>
2023-02-19 21:16:42 +0900

    Solved

---
+++ ex7-2/ex7-2/ex7-2.cpp
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+
+int main()
+{
+	int a[11], f;
+
+	for (int i = 1; i <= 10; i++)
+		cin >> a[i];
+	cin >> f;
+
+	cout << count(a + 1, a + 11, f) << "\n";
+
+	return 0;
+}
