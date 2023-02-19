7e0c7676cd6d427a8f45ab3a3ab3fdf6c09e65c3
PAMCH <pamic7124@naver.com>
2023-02-19 12:02:08 +0900

    Solved

---
+++ ex3-1/ex3-1/ex3-1.cpp
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+namespace mystd {
+	int count(int* first, int* last, const int &val) {
+		int cnt = 0;
+		while (first != last)
+		{
+			if (*first == val) {
+				cnt++;
+			}
+			first++;
+		}
+		return cnt;
+	}
+}
+
+int main()
+{
+	int a[101];
+	int n, s, e, k;
+
+	cin >> n >> s >> e >> k;
+
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+	}
+
+	cout << mystd::count(a + s, a + e + 1, k) << endl;
+
+	return 0;
+}
