6c3e970cd9905c5a1895a392be2595de6d02ffc1
PAMCH <pamic7124@naver.com>
2023-02-19 20:20:50 +0900

    Solved

---
+++ OOJ_3113/OOJ_3113/OOJ_3113.cpp
@@ -0,0 +1,37 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#define N 100000
+
+using namespace std;
+
+int n, L, k, a[N + 1];
+
+void input() {
+	cin >> n >> L >> k;
+
+	for (int i = 1; i <= n; i++)
+		cin >> a[i];
+}
+
+void core() {
+	auto cnt = 0;
+
+	sort(a + 1, a + n + 1);
+
+	for (int i = 1; i <= n && a[i] <= L; i++){
+		cnt++;
+		L += k;
+	}
+
+	cout << cnt << "\n";
+}
+
+int main()
+{
+	input();
+	core();
+
+	return 0;
+}
