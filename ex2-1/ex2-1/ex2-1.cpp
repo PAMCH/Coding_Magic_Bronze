c63e3d77eb487f2aa77d618bd48a82c38892501a
PAMCH <pamic7124@naver.com>
2023-02-19 10:36:59 +0900

    Solved

---
+++ ex2-1/ex2-1/ex2-1.cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARINIGS
+#include <iostream>
+
+int main()
+{
+    int year, month, day;
+
+    std::cin >> year >> month >> day;
+
+    std::cout.width(4);
+    std::cout.fill('0');
+    std::cout << year << "-";
+
+    std::cout.width(2);
+    std::cout.fill('0');
+    std::cout << month << "-";
+
+    std::cout.width(2);
+    std::cout.fill('0');
+    std::cout << day << std::endl;
+
+    return 0;
+}
