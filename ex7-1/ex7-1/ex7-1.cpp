a0859ca526fbc2aae0fabee822406e24e1dcea6f
PAMCH <pamic7124@naver.com>
2023-02-19 21:14:33 +0900

    Solved

---
+++ ex7-1/ex7-1/ex7-1.cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int a[11], f;
+
+	for (int i = 1; i <= 10; i++)
+		cin >> a[i];
+	cin >> f;
+
+	auto* p = find(a + 1, a + 11, f);
+
+	if (p == a + 11)
+		cout << "0" << "\n";
+	else
+		cout << p - a << "\n";
+
+	return 0;
+}
