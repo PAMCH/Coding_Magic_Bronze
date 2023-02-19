a2b572a7d7465eb201bf72e01c90281897b9c6a0
PAMCH <pamic7124@naver.com>
2023-02-19 18:40:26 +0900

    Solved

---
+++ OOJ_4034/OOJ_4034/OOJ_4034.cpp
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int s;
+long long d;
+
+void input() {
+	cin >> s >> d;
+}
+
+void core() {
+	long long day = 0;
+
+	while (true)
+	{
+		day += s;
+		if (d <= day)
+			break;
+		s++;
+	}
+}
+
+void output() {
+	cout << s << "\n";
+}
+
+int main()
+{
+	input();
+	core();
+	output();
+
+
+	return 0;
+}
