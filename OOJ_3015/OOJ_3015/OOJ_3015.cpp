de902477514bfeee01b8da9098ed59c7ee9e3754
PAMCH <pamic7124@naver.com>
2023-02-19 19:34:11 +0900

    Solved

---
+++ OOJ_3015/OOJ_3015/OOJ_3015.cpp
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#define N 50000
+
+using namespace std;
+
+int n, k;
+double a[N + 1];
+
+void input() {
+	cin >> n;
+	for (auto i = 1; i <= n; i++)
+		cin >> a[i];
+	cin >> k;
+}
+
+void core() {
+	nth_element(a + 1, a + k, a + n + 1, greater<double>());
+}
+
+void output() {
+	cout.precision(2);
+	cout << fixed << a[k] << "\n";
+}
+
+int main()
+{
+	input();
+	core();
+	output();
+
+	return 0;
+}
