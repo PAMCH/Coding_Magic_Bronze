86dc318680a0a3ee245cd10b6f1ee9191e63b345
PAMCH <pamic7124@naver.com>
2023-02-19 18:44:09 +0900

    Solved

---
+++ ex5-1/ex5-1/ex5-1.cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+auto max2(double x, double y) -> int {
+	return (x > y) ? x : y;
+}
+
+int main()
+{
+	auto sum = 0;
+
+	for (auto i = 1; i <= 10; i++) {
+		sum += i;
+	}
+
+	auto x = max2(10.5, 20.5);
+	cout << x << "\n";
+
+	return 0;
+}
