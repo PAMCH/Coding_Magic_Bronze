802dd7f3acb52a4d2060c35c7526f48f8612b6a5
PAMCH <pamic7124@naver.com>
2023-02-19 10:40:10 +0900

    Solved

---
+++ ex2-2/ex2-2/ex2-2.cpp
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARINIGS
+#include <iostream>
+
+int main()
+{
+    double money;
+
+    std::cin >> money;
+
+    for (int i = 1; i <= 5; i++) {
+        money *= 1.05;
+        std::cout.precision(2);
+        std::cout << std::fixed;
+        std::cout << round(money * 100) / 100.0 << std::endl;
+    }
+
+    return 0;
+}
