a4f2dd71069263129a92bf6df1976e7b7b48c275
PAMCH <pamic7124@naver.com>
2023-02-19 21:20:14 +0900

    Solved

---
+++ OOJ_4085/OOJ_4085/OOJ_4085.cpp
@@ -0,0 +1,37 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#define N 20000
+
+using namespace std;
+
+int n, a[N + 1];
+
+void input() {
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		cin >> a[i];
+}
+
+void core() {
+	auto sum = 0;
+
+	sort(a + 1, a + 1 + n);
+
+	for (auto i = 1; i <= n; i++)
+		sum += abs(a[n / 2 + 1] - a[i]);
+	cout << sum << "\n";
+}
+
+void output() {
+
+}
+
+int main()
+{
+	input();
+	core();
+
+	return 0;
+}
