a3d1ef35952bed78e61e76b57b97bedc5cee214f
PAMCH <pamic7124@naver.com>
2023-02-19 19:24:25 +0900

    Solved

---
+++ ex6-3/ex6-3/ex6-3.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int a[11], k;
+
+	for (auto i = 1l; i <= 10; i++)
+		cin >> a[i];
+	cin >> k;
+
+	nth_element(a + 1, a + k + 1, a + 11, greater<>());
+	
+	cout << a[k] << "\n";
+
+	return 0;
+}
