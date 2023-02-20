ac5b1292f88e8abab5717bc562c765d3b2933d98
PAMCH <pamic7124@naver.com>
2023-02-20 23:12:25 +0900

    Solved

---
+++ OOJ_1055/OOJ_1055/OOJ_1055.cpp
@@ -0,0 +1,50 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <string>
+#define N 100
+
+using namespace std;
+
+string a[N + 1];
+int n;
+char c;
+
+void input()
+{
+	cin >> n >> c;
+
+	c = tolower(c);
+
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> a[i];
+		for (auto& ch : a[i])
+		{
+			ch = tolower(ch);
+		}
+	}
+}
+
+void core()
+{
+	int cnt = 0;
+
+	for(int i=1;i<=n;i++)
+		if (a[i].front() == c)
+		{
+			cout << a[i] << endl;
+			cnt++;
+		}
+
+	if (!cnt)
+		cout << "NOT FOUND" << endl;
+}
+
+int main()
+{
+	input();
+	core();
+
+	return 0;
+}
