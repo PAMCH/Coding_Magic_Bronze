eec286a43d57d47b0bcdce5aec184d4838a9aef5
PAMCH <pamic7124@naver.com>
2023-02-20 23:07:28 +0900

    Solved

---
+++ OOJ_1054/OOJ_1054/OOJ_1054.cpp
@@ -0,0 +1,47 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <string>
+#define N 5
+
+using namespace std;
+
+string a[N + 1];
+char b[N + 1];
+
+
+void input()
+{
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> a[i] >> b[i];
+		
+		for (auto& ch : a[i])
+		{
+			ch = toupper(ch);
+		}
+		b[i] = toupper(b[i]);
+	}
+}
+
+void core()
+{
+	for (int i = 1; i <= N; i++)
+	{
+		int idx = a[i].find(b[i]);
+		cout << idx + 1 << endl;
+	}
+}
+
+void output()
+{
+
+}
+
+int main()
+{
+	input();
+	core();
+
+	return 0;
+}
