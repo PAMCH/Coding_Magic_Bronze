0fa3793d493a6cf6a07ed47e441d0611b33f0f61
PAMCH <pamic7124@naver.com>
2023-02-20 23:14:23 +0900

    Solved

---
+++ OOJ_1056/OOJ_1056/OOJ_1056.cpp
@@ -0,0 +1,50 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <string>
+#define N 100
+
+using namespace std;
+
+string a[N + 1], str;
+int n;
+
+void input()
+{
+	cin >> n >> str;
+
+	for (auto& ch : str)
+		ch = tolower(ch);
+
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> a[i];
+		for (auto& ch : a[i])
+		{
+			ch = tolower(ch);
+		}
+	}
+}
+
+void core()
+{
+	int cnt = 0;
+
+	for (int i = 1; i <= n; i++)
+		if (a[i].find(str) == 0)
+		{
+			cout << a[i] << endl;
+			cnt++;
+		}
+
+	if (!cnt)
+		cout << "NOT FOUND" << endl;
+}
+
+int main()
+{
+	input();
+	core();
+
+	return 0;
+}
