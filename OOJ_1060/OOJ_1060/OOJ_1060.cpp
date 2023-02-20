00889067d48c878bf4fcbda381f7f321f8e2aa3e
PAMCH <pamic7124@naver.com>
2023-02-20 23:29:25 +0900

    Solved

---
+++ OOJ_1060/OOJ_1060/OOJ_1060.cpp
@@ -0,0 +1,37 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+string str;
+
+void input()
+{
+	cin >> str;
+}
+
+void core()
+{
+	int koi = 0, ioi = 0, len = str.length();
+
+	for (int i = 0; i <= len - 3; i++)
+	{
+		if (str.substr(i, 3) == string("KOI"))
+			koi++;
+		if (str.substr(i, 3) == string("IOI"))
+			ioi++;
+	}
+
+	cout << koi << endl << ioi << endl;
+}
+
+
+int main()
+{
+	input();
+	core();
+
+	return 0;
+}
