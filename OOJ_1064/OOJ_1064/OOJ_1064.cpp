806ee5520418bc7d84531f8ea4478c53cc611954
PAMCH <pamic7124@naver.com>
2023-02-20 23:26:31 +0900

    Solved

---
+++ OOJ_1064/OOJ_1064/OOJ_1064.cpp
@@ -0,0 +1,42 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <string>
+#define N 100
+
+using namespace std;
+
+int n;
+string a[N + 1];
+
+void input()
+{
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+		cin >> a[i];
+}
+
+void output()
+{
+	for (int i = 1; i <= n; i++)
+	{
+		char ch = a[i].front();
+		if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+		{
+			cout << a[i] << "eluer" << endl;
+		}
+		else
+		{
+			cout << a[i].substr(1) << ch << "edu" << endl;
+		}
+	}
+}
+
+int main()
+{
+	input();
+	output();
+
+	return 0;
+}
