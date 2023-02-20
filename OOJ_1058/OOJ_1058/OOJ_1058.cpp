655d5295f9591881a14ae648877ad508fc469aa8
PAMCH <pamic7124@naver.com>
2023-02-20 23:16:45 +0900

    Solved

---
+++ OOJ_1058/OOJ_1058/OOJ_1058.cpp
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+string str;
+
+void input()
+{
+	cin >> str;
+}
+
+void core()
+{
+	int idx = (str.length() - 1) % 3 + 1;
+
+	for (int i = idx; i < str.length(); i += 4)
+		str.insert(i, ",");
+
+	cout << str << endl;
+}
+
+int main()
+{
+	input();
+	core();
+
+	return 0;
+}
