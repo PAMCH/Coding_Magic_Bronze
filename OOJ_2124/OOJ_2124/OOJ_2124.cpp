a9e41ed94bf6d735673d1c7138920b34761b281f
PAMCH <pamic7124@naver.com>
2023-02-20 21:08:14 +0900

    Solved

---
+++ OOJ_2124/OOJ_2124/OOJ_2124.cpp
@@ -0,0 +1,42 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int r, c, a, b;
+
+void input()
+{
+	cin >> r >> c;
+	cin >> a >> b;
+}
+
+void output()
+{
+	for (int i = 1; i <= r; i++)
+	{
+		for (int ii = 1; ii <= a; ii++)
+		{
+			for (int j = 1; j <= c; j++)
+			{
+				for (int jj = 1; jj <= b; jj++)
+				{
+					if ((i + j) % 2 == 0)
+						cout << "X";
+					else
+						cout << ".";
+				}
+			}
+			cout << "\n";
+		}
+	}
+}
+
+int main()
+{
+	input();
+	output();
+
+	return 0;
+}
