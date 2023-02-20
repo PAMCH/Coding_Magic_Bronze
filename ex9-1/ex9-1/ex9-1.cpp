22168c099759cc23c84a61f84ba4ec9eb8024a03
PAMCH <pamic7124@naver.com>
2023-02-20 21:26:30 +0900

    Solved

---
+++ ex9-1/ex9-1/ex9-1.cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+
+int main()
+{
+	int a[11], s, e;
+
+	for (int i = 1; i <= 10; i++)
+		cin >> a[i];
+	cin >> s >> e;
+
+	reverse(a + s, a + e + 1);
+
+	for (int i = 1; i <= 10; i++)
+		cout << a[i] << " ";
+	cout << "\n";
+
+	return 0;
+}
