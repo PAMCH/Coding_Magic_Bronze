2f18fb54c0aff53e83f73dd7099857fbaacca716
PAMCH <pamic7124@naver.com>
2023-02-20 22:52:45 +0900

    Solved

---
+++ ex11-3/ex11-3/ex11-3.cpp
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string x, str;
+
+	int n, sum = 0;
+
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> x;
+		sum += stoi(x);
+		str += x;
+	}
+
+	cout << sum << "\n";
+
+	reverse(str.begin(), str.end());
+	cout << str << endl;
+
+	return 0;
+}
