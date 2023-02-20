ed97948d197093a5d0e04ba6978af7c93e8541e0
PAMCH <pamic7124@naver.com>
2023-02-20 21:56:37 +0900

    Solved

---
+++ ex10-2/ex10-2/ex10-2.cpp
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int a[11];
+
+	for (int i = 1; i <= 10; i++)
+		cin >> a[i];
+
+	cout << *max_element(a + 1, a + 10 + 1) << "\n";
+	cout << max_element(a + 1, a + 10 + 1) -a<< "\n";
+
+
+	return 0;
+}
