e9e9cf8a3e063f9b0d1b35c16836be72d983eee9
PAMCH <pamic7124@naver.com>
2023-02-20 22:49:21 +0900

    Solved

---
+++ ex11-1/ex11-1/ex11-1.cpp
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	string str;
+
+	cin >> str;
+	cout << str.c_str() << "\n";
+
+	return 0;
+}
