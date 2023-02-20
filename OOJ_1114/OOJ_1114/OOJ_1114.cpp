97daf5dec3f4a83247df7d94608b4107142a0fb0
PAMCH <pamic7124@naver.com>
2023-02-20 23:00:47 +0900

    Solved

---
+++ OOJ_1114/OOJ_1114/OOJ_1114.cpp
@@ -0,0 +1,37 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <string>
+#define N 100
+
+using namespace std;
+
+int n;
+string a[N + 1];
+
+void input()
+{
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		cin >> a[i];
+}
+
+void output()
+{
+	for (int i = 1; i <= n; i++)
+	{
+		int len = a[i].length();
+		if (len > 10)
+			cout << a[i].front() << len - 2 << a[i].back() << endl;
+		else
+			cout << a[i] << endl;
+	}
+}
+
+int main()
+{
+	input();
+	output();
+
+	return 0;
+}
