acf571ee361f133af823f4f8e3136af3a6dea6e0
PAMCH <pamic7124@naver.com>
2023-02-20 20:59:49 +0900

    Solved

---
+++ ex8-1/ex8-1/ex8-1.cpp
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void prime(const int& x)
+{
+	int cnt = 0;
+
+	for (int i = 1; i <= x; i++)
+	{
+		if (x % i == 0)
+		{
+			cnt++;
+		}
+	}
+
+	if (cnt == 2)
+	{
+		cout << x << " ";
+	}
+
+}
+
+
+int main()
+{
+	int a[11];
+
+	for (int i = 1; i <= 10; i++)
+	{
+		cin >> a[i];
+	}
+
+	for_each(a + 1, a + 11, prime);
+
+	cout << "\n";
+
+	return 0;
+}
