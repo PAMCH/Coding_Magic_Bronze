9addd509e62ab58ad9aa84bc4769b57da686c199
PAMCH <pamic7124@naver.com>
2023-02-20 21:52:23 +0900

    Solved

---
+++ ex10-1/ex10-1/ex10-1.cpp
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+
+	cin >> a >> b >> c;
+
+	cout << min(min(a, b), c) << "\n";
+	cout << max(max(a, b), c) << "\n";
+
+
+	return 0;
+}
