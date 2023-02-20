ad674d4e8d89f27ce5ac2f32d3ced30b8628a412
PAMCH <pamic7124@naver.com>
2023-02-20 22:56:07 +0900

    Solved

---
+++ OOJ_1053/OOJ_1053/OOJ_1053.cpp
@@ -0,0 +1,40 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, even, odd;
+
+void input()
+{
+	string str;
+
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> str;
+		if (str.length() % 2 == 0)
+		{
+			even++;
+		}
+		else
+		{
+			odd++;
+		}
+	}
+}
+
+void output()
+{
+	cout << even << endl << odd << endl;
+}
+
+int main()
+{
+	input();
+	output();
+
+	return 0;
+}
