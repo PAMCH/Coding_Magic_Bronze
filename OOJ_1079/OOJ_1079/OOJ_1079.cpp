743c14c1dcf4efc9c6de51848c93ccabfa7402bf
PAMCH <pamic7124@naver.com>
2023-02-20 23:20:52 +0900

    Solved

---
+++ OOJ_1079/OOJ_1079/OOJ_1079.cpp
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+string str;
+
+void input()
+{
+	cin >> str;
+}
+
+void output()
+{
+	for (char i = 'Z'; i >= 'A'; i--)
+	{
+		int idx = str.find(i);
+
+		if (idx != -1)
+		{
+			str.erase(idx, 1);
+			cout << str << endl;
+		}
+	}
+}
+
+int main()
+{
+	input();
+	output();
+
+	return 0;
+}
