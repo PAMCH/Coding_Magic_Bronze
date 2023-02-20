5be88f9a869108bcac74fe0aedd80e821422f4b9
PAMCH <pamic7124@naver.com>
2023-02-20 23:18:24 +0900

    Solved

---
+++ OOJ_1092/OOJ_1092/OOJ_1092.cpp
@@ -0,0 +1,33 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <string>
+#define N 100
+
+using namespace std;
+
+int n;
+string str;
+
+void input()
+{
+	cin >> n >> str;
+}
+
+void output()
+{
+	str.erase(n - 1, 1);
+
+	if (str.empty())
+		cout << -1 << endl;
+	else
+		cout << str << endl;
+}
+
+int main()
+{
+	input();
+	output();
+
+	return 0;
+}
