fe6d6017172c5c4778f4521ec3bccf36ad7ce7ab
PAMCH <pamic7124@naver.com>
2023-02-21 21:49:10 +0900

    Solved

---
+++ OOJ_2122/OOJ_2122/OOJ_2122.cpp
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+string str, res;
+
+void input()
+{
+	cin >> str;
+}
+
+void core()
+{
+	for (const auto& ch : str)
+	{
+		int idx = string("CAMBRIDGE").find(ch);
+		if (idx == -1)
+			res.push_back(ch);
+	}
+}
+
+void output()
+{
+	cout << res << endl;
+}
+
+int main()
+{
+	input();
+	core();
+	output();
+
+	return 0;
+}
