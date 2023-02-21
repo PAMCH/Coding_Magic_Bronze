82c823b1c0ad0968ed4d5868a6406f2373998819
PAMCH <pamic7124@naver.com>
2023-02-21 21:46:59 +0900

    Solved

---
+++ OOJ_2052/OOJ_2052/OOJ_2052.cpp
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+string str;
+char a[17][6];
+int n;
+
+void input()
+{
+	cin >> str;
+	n = str.length() / 5;
+}
+
+void core()
+{
+	for (int j = 1; j <= 5; j++)
+		for (int i = 1; i <= n; i++)
+			a[i][j] = str[(j - 1) * n + (i - 1)];
+}
+
+void output()
+{
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= 5; j++)
+			cout << a[i][j];
+	cout << endl;
+}
+
+int main()
+{
+	input();
+	core();
+	output();
+
+	return 0;
+}
