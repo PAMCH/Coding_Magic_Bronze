2996e777df3323b497812a1c211a2b5104c8087c
PAMCH <pamic7124@naver.com>
2023-02-21 21:08:41 +0900

    Solved

---
+++ ex12-6/ex12-6/ex12-6.cpp
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+string str, y, m, d;
+
+void input()
+{
+	cin >> str;
+}
+
+void core()
+{
+	y = str.substr(0, 4);
+	m = str.substr(4, 2);
+	d = str.substr(6, 2);
+}
+
+void output()
+{
+	cout << y << "-" << m << "-" << d << "\n";
+}
+
+int main()
+{
+	input();
+	core();
+	output();
+
+	return 0;
+}
