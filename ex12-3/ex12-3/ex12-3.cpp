9f111cdc211cfacff57c8dfc1572b1da44bdc6e9
PAMCH <pamic7124@naver.com>
2023-02-21 20:41:54 +0900

    Solved

---
+++ ex12-3/ex12-3/ex12-3.cpp
@@ -0,0 +1,33 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+string str1, str2;
+
+void input()
+{
+	cin >> str1;
+	cin >> str2;
+}
+
+void core()
+{
+	if (str1 < str2)
+		cout << "Negative" << endl;
+	else if (str1 > str2)
+		cout << "Positive" << endl;
+	else
+		cout << "Zero" << endl;
+}
+
+int main()
+{
+	input();
+	core();
+
+	return 0;
+}
