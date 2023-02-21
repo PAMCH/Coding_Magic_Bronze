0c2574e01268ab413e17e540629908c2dd9fba88
PAMCH <pamic7124@naver.com>
2023-02-21 21:26:18 +0900

    Solved

---
+++ ex12-10/ex12-10/ex12-10.cpp
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int n, idx=0;
+string str[101];
+
+void input()
+{
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> str[i];
+		if (str[idx].length() < str[i].length())
+			idx = i;
+	}
+}
+
+void core()
+{
+	cout << str[idx] << endl;
+}
+
+int main()
+{
+	input();
+	core();
+
+	return 0;
+}
