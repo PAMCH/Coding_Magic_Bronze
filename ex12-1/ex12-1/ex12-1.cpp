aba534179ea5eb343b16e6eee0840a06fe30daab
PAMCH <pamic7124@naver.com>
2023-02-21 20:33:10 +0900

    Solved

---
+++ ex12-1/ex12-1/ex12-1.cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+
+
+int main()
+{
+	string str;
+
+	cin >> str;
+
+	if (str.length() % 2)
+		cout << "odd" << endl;
+	else
+		cout << "even" << endl;
+
+	return 0;
+}
