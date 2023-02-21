3aeef89d5aac0c9ff00eda57a141ab29d83090b5
PAMCH <pamic7124@naver.com>
2023-02-21 21:04:55 +0900

    Solved

---
+++ ex12-5/ex12-5/ex12-5.cpp
@@ -0,0 +1,28 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+string str;
+
+void input()
+{
+	cin >> str;
+}
+
+void core()
+{
+	for (int i = 0; i < str.length(); i++)
+		cout << str.substr(i) << endl;
+}
+
+int main()
+{
+	input();
+	core();
+
+	return 0;
+}
