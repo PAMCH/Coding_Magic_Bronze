31e1a617cc6bdf1a227268f6b9801c31524b0068
PAMCH <pamic7124@naver.com>
2023-02-21 21:23:18 +0900

    Solved

---
+++ ex12-9/ex12-9/ex12-9.cpp
@@ -0,0 +1,36 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+string str[101];
+int n;
+
+void input()
+{
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> str[i];
+
+		for (auto& ch : str[i])
+			ch = toupper(ch);
+	}
+}
+
+void core()
+{
+	for (int i = 1; i <= n; i++)
+		cout << str[i].substr(str[i].length() - 3) << endl;
+}
+
+int main()
+{
+	input();
+	core();
+
+	return 0;
+}
