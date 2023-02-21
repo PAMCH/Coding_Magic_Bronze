241df6cfaab0dd1d95069ab866ccad8865214c31
PAMCH <pamic7124@naver.com>
2023-02-21 20:37:26 +0900

    Solved

---
+++ ex12-2/ex12-2/ex12-2.cpp
@@ -0,0 +1,33 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+string str;
+
+void input()
+{
+	cin >> str;
+}
+
+void core()
+{
+	reverse(str.begin(), str.end());
+}
+
+void output()
+{
+	cout << str << endl;
+}
+
+int main()
+{
+	input();
+	core();
+	output();
+
+	return 0;
+}
