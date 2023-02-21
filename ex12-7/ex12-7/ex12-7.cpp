6db62e00aec8518d134fac912580ba5c0b2bca7a
PAMCH <pamic7124@naver.com>
2023-02-21 21:11:08 +0900

    Solved

---
+++ ex12-7/ex12-7/ex12-7.cpp
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+string str1, str2;
+
+void input()
+{
+	cin >> str1 >> str2;
+}
+
+void core()
+{
+	if (str1.find(str2) == -1)
+		cout << 0 << endl;
+	else
+		cout << str1.substr(str1.find(str2)) << endl;
+}
+
+
+int main()
+{
+	input();
+	core();
+
+	return 0;
+}
