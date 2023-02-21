c9e0b415af46bf88e324680b9341f2a1f81b935b
PAMCH <pamic7124@naver.com>
2023-02-21 20:55:57 +0900

    Solved

---
+++ ex12-4/ex12-4/ex12-4.cpp
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+string str1, str2;
+
+void input()
+{
+	cin >> str1;
+	cin >> str2;
+}
+
+void core()
+{
+	cout << str1 << ", " << str2 << endl;
+	swap(str1, str2);
+	cout << str1 << ", " << str2 << endl;
+}
+
+int main()
+{
+	input();
+	core();
+
+	return 0;
+}
