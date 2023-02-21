c13f31dc8ea1e879e2481b6224fa5df702983d47
PAMCH <pamic7124@naver.com>
2023-02-21 21:17:14 +0900

    Solved

---
+++ ex12-8/ex12-8/ex12-8.cpp
@@ -0,0 +1,36 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int n;
+string str[101];
+
+void input()
+{
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> str[i];
+		reverse(str[i].begin(), str[i].end());
+	}
+}
+
+void core()
+{
+	for (int i = 1; i <= n; i++)
+	{
+		cout << str[i] << endl;
+	}
+}
+
+int main()
+{
+	input();
+	core();
+
+	return 0;
+}
