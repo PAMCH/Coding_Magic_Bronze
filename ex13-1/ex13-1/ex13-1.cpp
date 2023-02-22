33863c7f8d63978df63c077b61b2a0153a266f41
PAMCH <pamic7124@naver.com>
2023-02-22 21:45:13 +0900

    Solved

---
+++ ex13-1/ex13-1/ex13-1.cpp
@@ -0,0 +1,45 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+
+
+using namespace std;
+
+pair<int, string> a[101];
+string name;
+int n, c, t;
+
+
+void input()
+{
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> name >> c >> t;
+		a[i] = { c + t,name };
+	}
+}
+
+void core()
+{
+	sort(a + 1, a + n + 1, greater<>());
+}
+
+void output()
+{
+	for (int i = 1; i <= n; i++)
+	{
+		cout << a[i].second << " " << a[i].first << endl;
+	}
+}
+
+int main()
+{
+	input();
+	core();
+	output();
+
+	return 0;
+}
