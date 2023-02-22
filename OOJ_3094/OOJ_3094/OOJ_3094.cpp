fed56fa7716895955795aaa10e522a15ed722103
PAMCH <pamic7124@naver.com>
2023-02-22 22:47:18 +0900

    Solved

---
+++ OOJ_3094/OOJ_3094/OOJ_3094.cpp
@@ -0,0 +1,49 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <tuple>
+#define N 50000
+
+using namespace std;
+
+int t, n;
+pair<int, int>a[N + 1];
+
+void input()
+{
+	int x;
+
+	cin >> t >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> x;
+		a[i] = { abs(x),x };
+	}
+}
+
+void core()
+{
+	int sum = 0, i;
+	sort(a + 1, a + n + 1);
+	for (i = 1; i <= n; i++)
+	{
+		int dist = abs(a[i].second - a[i - 1].second);
+		if (t < sum + dist)
+		{
+			break;
+		}
+		sum += dist;
+	}
+	cout << i-1 << endl;
+}
+
+int main()
+{
+	input();
+	core();
+
+	return 0;
+}
