eed58a7d2d18e613a9d7a1abf23317d37a593eac
PAMCH <pamic7124@naver.com>
2023-02-23 22:51:22 +0900

    Solved

---
+++ OOJ_3112/OOJ_3112/OOJ_3112.cpp
@@ -0,0 +1,55 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <tuple>
+#define N 25000
+
+using namespace std;
+
+int n;
+tuple<double, int, int>a[N + 1];
+
+void input()
+{
+	int j, p;
+
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> j >> p;
+		a[i] = { double(j) / p,p,i };
+	}
+}
+
+void core()
+{
+	partial_sort(a + 1, a + 4, a + n + 1, greater<tuple<double, int, int>>());
+}
+
+void output()
+{
+	int sum = 0;
+
+	for (int i = 1; i <= 3; i++)
+	{
+		sum += get<1>(a[i]);
+	}
+
+	cout << sum << endl;
+
+	for (int i = 1; i <= 3; i++)
+	{
+		cout << get<2>(a[i]) << endl;
+	}
+}
+
+int main()
+{
+	input();
+	core();
+	output();
+
+	return 0;
+}
