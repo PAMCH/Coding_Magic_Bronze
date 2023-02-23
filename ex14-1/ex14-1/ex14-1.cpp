c2e6d1d5120d92a80a05e50e7061d3d4e52da80c
PAMCH <pamic7124@naver.com>
2023-02-23 22:28:12 +0900

    Solved

---
+++ ex14-1/ex14-1/ex14-1.cpp
@@ -0,0 +1,45 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <tuple>
+
+
+using namespace std;
+
+tuple<int, int, string>a[101];
+string name;
+int n, c, t;
+
+void input()
+{
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> name >> c >> t;
+		a[i] = {c,t,name};
+	}
+}
+
+void core()
+{
+	sort(a + 1, a + n + 1, greater<>());
+}
+
+void output()
+{
+	for (int i = 1; i <= n; i++)
+	{
+		cout << get<2>(a[i]) << " " << get<0>(a[i]) << " " << get<1>(a[i]) << endl;
+	}
+}
+
+int main()
+{
+	input();
+	core();
+	output();
+
+	return 0;
+}
