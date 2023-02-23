c5056aba923647a4ad8f9c1ec6f06270e98cbf39
PAMCH <pamic7124@naver.com>
2023-02-23 23:08:01 +0900

    Solved

---
+++ ex15-1/ex15-1/ex15-1.cpp
@@ -0,0 +1,44 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <tuple>
+#include <stack>
+
+using namespace std;
+
+int n, x;
+stack<int>S;
+
+void input()
+{
+	cin >> n;
+}
+
+void core()
+{
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> x;
+		S.push(x);
+	}
+}
+
+void output()
+{
+	while (!S.empty())
+	{
+		cout << S.top() << endl;
+		S.pop();
+	}
+}
+
+int main()
+{
+	input();
+	core();
+	output();
+
+	return 0;
+}
